754771d076d585e2af747c56332443eae071d6a9
Krzysztof Nowicki <krissn@op.pl>
2015-10-23 22:30:50 +0200

    Update translation template

--- src/lang/pl_PL.ts
+++ src/lang/pl_PL.ts
@@ -2,25 +2,86 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="pl_PL">
 <context>
-    <name>qworship</name>
+    <name>LyricsSection</name>
     <message>
-        <source>Main Window</source>
-        <translation>Okno główne</translation>
+        <location filename="../LyricsSection.cpp" line="128"/>
+        <source>Invalid character in opening tag name: </source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../LyricsSection.cpp" line="135"/>
+        <location filename="../LyricsSection.cpp" line="183"/>
+        <location filename="../LyricsSection.cpp" line="212"/>
+        <source>Reached end of string before tag close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LyricsSection.cpp" line="146"/>
+        <source>Incorrect tag parameter.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LyricsSection.cpp" line="274"/>
+        <source>Unknown tag: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LyricsSection.cpp" line="176"/>
+        <source>Invalid character in closing tag name: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LyricsSection.cpp" line="79"/>
+        <source>Unmatched tag closure at top level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LyricsSection.cpp" line="188"/>
+        <source>Tag opening/closure mismatch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LyricsSection.cpp" line="219"/>
+        <source>Empty tag name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LyricsSection.cpp" line="246"/>
+        <location filename="../LyricsSection.cpp" line="256"/>
+        <source>This tag requires an argument</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LyricsSection.cpp" line="262"/>
+        <source>Font size out of range (1-9)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>qworship</name>
+    <message>
+        <location filename="../qml/qworship.qml" line="6"/>
+        <source>Main Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/qworship.qml" line="43"/>
         <source>LIVE</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../qml/qworship.qml" line="44"/>
         <source>SEQUENCE</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>SETTINGS</source>
+        <location filename="../qml/qworship.qml" line="45"/>
+        <source>LYRICS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>LYRICS</source>
+        <location filename="../qml/qworship.qml" line="46"/>
+        <source>SETTINGS</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
